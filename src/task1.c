9d9ece7b43f789fe97c0209ca1b13798ebed6b68
Alex Hoffman <alxhoff@gmail.com>
2019-04-16 13:40:11 +0200

    changed task1

--- src/task1.c
+++ src/task1.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 
-#include "espl_lib.h"
 
 int main(){
     int num;
     char a;
-    varchar(100) str;
-a:= 'i';
+    char str;
+a = 'i';
 while(a = 'i') 
     {
  	printf("Please input a number\n");
         scanf("%d", &num);
-    str := num_to_words(num) ;
-        printf("%c\n",str);
+    str = num_to_words(num) ;
+        printf("%s\n",str);
         printf("type x to exist or i to input another number\n");
         scanf("%c", &a);
     }
