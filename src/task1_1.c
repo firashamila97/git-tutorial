551f7f0700a72159dee0b6780e6bffede4580463
Lab ESPL Stud05 lab_espl_stud05 <lab_espl_stud05@tueircs-ldb06.int.rcs.ei.tum.de>
2019-11-06 15:48:06 +0100

    exercice 1.1

--- src/task1_1.c
+++ src/task1_1.c
@@ -3,19 +3,25 @@
 #include "espl_lib.h"
 
 int main(){
-    int num;
-    char a;
-    varchar(100) str;
-a:= 'i';
-while(a = 'i') 
+    int num,a;
+    char b;
+    
+
+ a=1;
+
+while(a!=0) 
     {
  	printf("Please input a number\n");
         scanf("%d", &num);
-    str := num_to_words(num) ;
-        printf("%c\n",str);
-        printf("type x to exist or i to input another number\n");
-        scanf("%c", &a);
+        getchar ();
+        printf("%s\n",num_to_words(num));
+        printf("type x to exist or e to enter another number\n");
+        scanf("%c", &b);
+        if (b == 'e') { 
+              a=1; }
+        else { 
+              a=0; }
     }
         
-	return 0;
+	
 }
